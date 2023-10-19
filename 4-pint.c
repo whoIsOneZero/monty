a9789f6592c38dba577224d5c1708a50fa075f4e
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-10-19 23:25:16 +0000

    docs: update for pint

--- 4-pint.c
+++ 4-pint.c
@@ -1,18 +1,18 @@
 #include "monty.h"
 /**
  * my_pint - prints value at top of the stack
- * @head: double pointer to the head of the stack.
+ * @top: double pointer to the head of the stack.
  * @line_num: the line number where opcode is
 */
-void my_pint(stack_t **head, unsigned int line_num)
+void my_pint(stack_t **top, unsigned int line_num)
 {
-	if (*head == NULL)
+	if (*top == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_num);
 		fclose(context.file);
 		free(context.content);
-		my_free_stack(*head);
+		my_free_stack(*top);
 		exit(EXIT_FAILURE);
 	}
-	printf("%d\n", (*head)->n);
+	printf("%d\n", (*top)->n);
 }
