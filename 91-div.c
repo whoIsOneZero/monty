fe2dc2c241880417d536a4225cd2ebdd525f2806
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-10-20 01:27:43 +0000

    docs: update for div

---
+++ 91-div.c
@@ -0,0 +1,40 @@
+#include "monty.h"
+/**
+ * my_div - finds the quotient of the top two elements of the stack. dd.
+ * @top: double pointer to the head of the stack.
+ * @line_num: line number where the opcode is.
+*/
+void my_div(stack_t **top, unsigned int line_num)
+{
+	stack_t *ptr;
+	int len = 0, temp;
+
+	ptr = *top;
+	while (ptr)
+	{
+		ptr = ptr->next;
+		len++;  /*Get the number of elements in the stack*/
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_num);
+		fclose(context.file);
+		free(context.content);
+		my_free_stack(*top);
+		exit(EXIT_FAILURE);
+	}
+	ptr = *top; /*Reassignment*/
+	if (ptr->n == 0)  /*Handle division by zero*/
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_num);
+		fclose(context.file);
+		free(context.content);
+		my_free_stack(*top);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = ptr->next->n / ptr->n;
+	ptr->next->n = temp;
+	*top = ptr->next;
+	free(ptr);
+}
